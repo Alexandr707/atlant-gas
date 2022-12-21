006e9fde73fc14753e442debb7e5dc066fb547ac
Alexandr707 <alexandralennikov90@gmail.com>
2022-12-21 21:56:19 +0700

    first commit

---
+++ src/main.ts
@@ -0,0 +1,71 @@
+import "./scss/style.scss";
+
+// import Swiper from "swiper";
+// import "swiper/swiper-bundle.css";
+import Swiper from "swiper/swiper-bundle.esm.js";
+
+const thumbs: HTMLElement = document.querySelector(".volum__thumbs")!;
+const galleryTop = new Swiper(".volume__galery", {
+  on: {
+    slideChange: function ({ activeIndex }: Swiper) {
+      for (const el of thumbs.children) {
+        el.classList.remove("active");
+        if (parseInt(el.dataset.num) === activeIndex)
+          el.classList.add("active");
+      }
+    },
+  },
+});
+
+if (thumbs) {
+  thumbs.addEventListener("click", function (e: Event) {
+    const target: keyof HTMLElement = e.target!;
+    const dl = target.closest("dl");
+    if (dl) {
+      for (const el of thumbs.children) {
+        el.classList.remove("active");
+      }
+    }
+    dl.classList.add("active");
+    galleryTop.slideTo(+dl.dataset.num);
+  });
+}
+
+const feedbackSlider = new Swiper(".feedback__swiper", {
+  effect: "cards",
+  cardsEffect: {
+    slideShadows: false,
+    rotate: false,
+  },
+
+  grabCursor: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".slide-next",
+    prevEl: ".slide-prev",
+  },
+});
+
+const orderBtns = document.querySelectorAll(".open-popap");
+
+if (orderBtns) {
+  orderBtns.forEach((b) => {
+    b.addEventListener("click", (e) => {
+      e.preventDefault();
+      document.body.classList.add("open");
+    });
+  });
+}
+
+const closePopap = document.querySelector(".popap__close");
+
+if (closePopap) {
+  closePopap.addEventListener("click", () => {
+    document.body.classList.remove("open");
+  });
+}
