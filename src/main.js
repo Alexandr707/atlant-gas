bf4138773cd2bacc33bf8cc21d1b86ff234e5646
Alexandr707 <alexandralennikov90@gmail.com>
2023-04-04 08:57:12 +0700

    activate script after dom loaded

--- src/main.js
+++ src/main.js
@@ -1,81 +1,83 @@
-import "./scss/style.scss";
+import './scss/style.scss';
 
-import Swiper from "swiper/swiper-bundle.esm.js";
-import WOW from "wow.js";
+if (document.readyState === 'complete') init();
+else document.addEventListener('DOMContentLoaded', init);
 
-const thumbs = document.querySelector(".volum__thumbs");
-const galleryTop = new Swiper(".volume__galery", {
-  on: {
-    slideChange: function ({ activeIndex }) {
-      for (const el of thumbs.children) {
-        el.classList.remove("active");
-        if (parseInt(el.dataset.num) === activeIndex)
-          el.classList.add("active");
-      }
+function init() {
+  const thumbs = document.querySelector('.volum__thumbs');
+  const galleryTop = new Swiper('.volume__galery', {
+    on: {
+      slideChange: function ({ activeIndex }) {
+        for (const el of thumbs.children) {
+          el.classList.remove('active');
+          if (parseInt(el.dataset.num) === activeIndex)
+            el.classList.add('active');
+        }
+      },
     },
-  },
-});
+  });
 
-if (thumbs) {
-  thumbs.addEventListener("click", function (e) {
-    const target = e.target;
-    const dl = target.closest("dl");
-    if (dl) {
-      for (const el of thumbs.children) {
-        el.classList.remove("active");
+  if (thumbs) {
+    thumbs.addEventListener('click', function (e) {
+      const target = e.target;
+      const dl = target.closest('dl');
+      if (dl) {
+        for (const el of thumbs.children) {
+          el.classList.remove('active');
+        }
       }
-    }
-    dl.classList.add("active");
-    galleryTop.slideTo(+dl.dataset.num);
-  });
-}
-
-const feedbackSlider = new Swiper(".feedback__swiper", {
-  effect: "cards",
-  cardsEffect: {
-    slideShadows: false,
-    rotate: false,
-  },
-
-  grabCursor: true,
-
-  pagination: {
-    el: ".swiper-pagination",
-    type: "bullets",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".slide-next",
-    prevEl: ".slide-prev",
-  },
-});
-
-const orderBtns = document.querySelectorAll(".open-popap");
-
-if (orderBtns) {
-  orderBtns.forEach((b) => {
-    b.addEventListener("click", (e) => {
-      e.preventDefault();
-      document.body.classList.add("open");
+      dl.classList.add('active');
+      galleryTop.slideTo(+dl.dataset.num);
     });
-  });
-}
+  }
 
-const closePopap = document.querySelector(".popap__close");
-const backplatePopap = document.querySelector(".popap__backpalte");
+  const feedbackSlider = new Swiper('.feedback__swiper', {
+    effect: 'cards',
+    cardsEffect: {
+      slideShadows: false,
+      rotate: false,
+    },
 
-if (closePopap) {
-  closePopap.addEventListener("click", () => {
-    document.body.classList.remove("open");
-  });
-}
-if (backplatePopap) {
-  backplatePopap.addEventListener("click", () => {
-    document.body.classList.remove("open");
-  });
-}
+    grabCursor: true,
 
-const wow = new WOW(/* {
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.slide-next',
+      prevEl: '.slide-prev',
+    },
+  });
+
+  const orderBtns = document.querySelectorAll('.open-popap');
+
+  if (orderBtns) {
+    orderBtns.forEach((b) => {
+      b.addEventListener('click', (e) => {
+        e.preventDefault();
+        document.body.classList.add('open');
+      });
+    });
+  }
+
+  const closePopap = document.querySelector('.popap__close');
+  const backplatePopap = document.querySelector('.popap__backpalte');
+
+  if (closePopap) {
+    closePopap.addEventListener('click', () => {
+      document.body.classList.remove('open');
+    });
+  }
+  if (backplatePopap) {
+    backplatePopap.addEventListener('click', () => {
+      document.body.classList.remove('open');
+    });
+  }
+
+  const wow = new WOW(/* {
   offset: 200,
 } */);
-wow.init();
+  wow.init();
+}
